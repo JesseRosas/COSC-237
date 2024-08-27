ab642e78196ee9fe0225607e448717258b9365d1
JesseRosas <Jessebrosas@yahoo.com>
2024-08-27 14:56:26 -0400

    Renamed Class

---
+++ practicum1.java
@@ -0,0 +1,10 @@
+public class practicum1 {
+  
+    public static void main(String[] args) {
+      java.util.Scanner input = new java.util.Scanner(System.in);
+  
+      System.out.println("What is your name? ");
+      String name = input.nextLine();
+      System.out.println("Hi " + name);
+    }
+  }
